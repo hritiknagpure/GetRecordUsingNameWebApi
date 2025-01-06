ce63aa8d4c99a5b7997927ba6bf72bdf9d3907d5
hriti <hriti@HRITIKNAGPURE>
2025-01-06 16:45:01 +0530

    Add project files.

---
+++ TurfBookingSystem/Controllers/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TurfBookingSystem.Models;
+
+namespace TurfBookingSystem.Controllers
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Currency> Currencies { get; set; }
+    }
+}
---
+++ TurfBookingSystem/Models/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TurfBookingSystem.Models
+{
+    public class Currency
+    {
+
+        [Key]
+        public int currencyId { get; set; }
+        //public string MyProperty { get; set; }
+        public string Mypropertyname { get; set; }
+    }
+}
